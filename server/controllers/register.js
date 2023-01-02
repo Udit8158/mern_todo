a00834c1ee773922919dde8889adc12d0d39f449
Udit8158 <kunduudit8158@gmail.com>
2023-01-02 18:05:18 +0530

    Do some changes

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,13 +1,29 @@
 import { validationResult } from "express-validator";
+import UserSchema from "../schemas/UserSchema.js";
 
-const register = (req, res) => {
+const register = async (req, res) => {
   const { name, email, password } = req.body;
 
-  // Check every filds are in the body or not
-  if (!name || !email || !password)
-    return res.status(400).json({ message: "Every filds are required" });
+  // checking validation error
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-  res.json(req.body);
+  // Check every filds are in the body or not
+  // if (!name || !email || !password)
+  //   return res.status(400).json({ message: "Every filds are required" });
+
+  // check for duplicate inside DB
+  const duplicate = await UserSchema.findOne({ email: email });
+  if (duplicate)
+    return res.status(400).json({ message: "User already exists" });
+
+  // Now save new user to database
+  const user = new UserSchema({ name, email, password });
+  await user.save();
+
+  res.json(user);
 };
 
 export default register;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,18 @@
 import express from "express";
+import { body } from "express-validator";
 import register from "../controllers/register.js";
-import validateUser from "../middlewares/validateUserInput.js";
 
 const authRoute = express();
 
-authRoute.route("/register").post(validateUser, register);
+authRoute
+  .route("/register")
+  .post(
+    [
+      body("name").exists(),
+      body("email").isEmail(),
+      body("password").isLength({ min: 6 }),
+    ],
+    register
+  );
 
 export default authRoute;
